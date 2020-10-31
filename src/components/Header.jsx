ae9415f254bc85d5ca63a8e144c0dfe09e25e97d
SeongyunLim <lsy9699@naver.com>
2020-10-31 15:14:02 +0900

    Stopwathch conditional btn implement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import Statistics from "./Statistics";
+import Stopwatch from "./Stopwatch";
 // eslint-disable-next-line no-undef
 export let Header = ({title, players} = props) => {
   // console.log(props)
@@ -8,6 +9,7 @@ export let Header = ({title, players} = props) => {
     <header className="header">
       <Statistics players={players}/>
       <h1 className="h1">{title}</h1>
+      <Stopwatch/>
 
     </header>
   );
---
+++ src/components/Stopwatch.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+class Stopwatch extends React.Component{
+  tickRef;
+  state = {
+    isRunning:false
+  }
+  //돔이 렌더링 된 직후에 호출
+  //REST API 호출, 서드 파티 라이브러리 로딩
+  componentDidMount() {
+    this.tickRef = setInterval(()=>{},1000);
+  }
+  getButton = () =>{
+    if(this.state.isRunning){
+      return (
+        <button onClick={()=>{this.setState({isRunning : ! this.state.isRunning})}}>stop</button>
+      )
+    } else {
+      return (
+        <button onClick={()=>{this.setState({isRunning : ! this.state.isRunning})}}>start</button>
+      )
+    }
+  }
+  render() {
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <span className="stopwatch-time">0</span>
+        {
+          this.getButton()
+        }
+
+        <button>Reset</button>
+      </div>
+    );
+  }
+  
+  //돔이 파괴되기 직전에 호출되는 라이프 사이클 메서드
+  componentWillUnmount() {
+    clearInterval(this.tickRef);
+  }
+
+}
+
+export default Stopwatch;
