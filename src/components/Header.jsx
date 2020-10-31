a1aab64c17147bf99d0eabbae3e330b4583680fe
SeongyunLim <lsy9699@naver.com>
2020-10-31 11:45:16 +0900

    Statistics UI 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 
 import React from "react";
+import Statistics from "./Statistics";
 // eslint-disable-next-line no-undef
 export let Header = ({title, players} = props) => {
   // console.log(props)
   return (
     <header className="header">
+      <Statistics/>
       <h1 className="h1">{title}</h1>
-      <span className="stats">Players : {players}</span>
+
     </header>
   );
 }
