fd824e84e5d30f3321fb615cef2e0de4a0932273
SeongyunLim <lsy9699@naver.com>
2020-10-31 14:45:35 +0900

    Add Player Form.jsx validation 수정

--- src/components/AddPlayerForm.jsx
+++ src/components/AddPlayerForm.jsx
@@ -4,25 +4,43 @@ class AddPlayerForm extends React.Component{
   state ={
     value : ''
   }
+  constructor(props) {
+    super(props);
+    this.formRef = React.createRef();
+    this.textRef = React.createRef();
 
-  handleValueChange = (e,param) =>{
-    this.setState(
-      {
-        value : e.target.value
-      }
-    );
+  }
+
+  handleValueChange = (e) =>{
+    this.setState({value: e.target.value});
   }
   handleSubmit =(e)=>{
     e.preventDefault();
     console.log('handleSubmit')
+    //text 노드에 접근 하는 방법
+    const form = this.formRef.current;
+    const player = this.textRef.current; //document.getElementById(id)
+    console.log(form.checkValidity()); //폼내의 모든 입력 validation을 체크
+    console.log(player.validity.valid) //입력의 9가지 validation 을 체크
+    if(!form.checkValidity()){
+      //invalid 폼을 찾아서 에러문구 표시
+      return;
+    }
+
     this.props.addPlayer(this.state.value);
     this.setState({value:''})
   }
   render() {
     return(
-      <form action="" className="form" onSubmit={this.handleSubmit}>
-        <input type="text" className="input" placeholder="enter a player's name" value={this.state.value} onChange={(e)=>this.handleValueChange(e,1)}/>
-        <input type="submit" className="input" value="Add Player"/>
+      <form ref={this.formRef} className="form" onSubmit={this.handleSubmit} noValidate>
+        <input type="text"
+               ref={this.textRef}
+               className="input"
+               placeholder="enter a player's name"
+               value={this.state.value}
+               onChange={this.handleValueChange}
+               required/>
+        <input  type="submit" className="input" value="Add Player"/>
       </form>
     );
   }
---
+++ src/js/thread1.js
@@ -0,0 +1,11 @@
+console.log("a")
+
+//예약
+setTimeout(()=>{
+  console.log("B")
+},0);
+
+console.log("C")
+
+//만일 루프가돌면?
+//while (true){}
