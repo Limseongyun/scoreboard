41336a89b5ba647013b2f5a85a329a93a5ff8caf
SeongyunLim <lsy9699@naver.com>
2020-10-31 17:52:41 +0900

    class Component -> function Component
    -AddPlayerForm

--- src/components/AddPlayerForm.jsx
+++ src/components/AddPlayerForm.jsx
@@ -1,25 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
-class AddPlayerForm extends React.Component{
-  state ={
-    value : ''
-  }
-  constructor(props) {
-    super(props);
-    this.formRef = React.createRef();
-    this.textRef = React.createRef();
+function AddPlayerForm(props){
+  const [value,setValue] = useState( '');
 
-  }
+  const formRef = React.createRef();
+  const textRef = React.createRef();
 
-  handleValueChange = (e) =>{
-    this.setState({value: e.target.value});
+
+  const handleValueChange = (e) =>{
+    setValue(e.target.value);
   }
-  handleSubmit =(e)=>{
+  const handleSubmit =(e)=>{
     e.preventDefault();
     console.log('handleSubmit')
     //text 노드에 접근 하는 방법
-    const form = this.formRef.current;
-    const player = this.textRef.current; //document.getElementById(id)
+    const form = formRef.current;
+    const player = textRef.current; //document.getElementById(id)
     console.log(form.checkValidity()); //폼내의 모든 입력 validation을 체크
     console.log(player.validity.valid) //입력의 9가지 validation 을 체크
     if(!form.checkValidity()){
@@ -27,23 +23,23 @@ class AddPlayerForm extends React.Component{
       return;
     }
 
-    this.props.addPlayer(this.state.value);
-    this.setState({value:''})
+    props.addPlayer(value);
+    setValue('');
   }
-  render() {
+
     return(
-      <form ref={this.formRef} className="form" onSubmit={this.handleSubmit} noValidate>
+      <form ref={formRef} className="form" onSubmit={handleSubmit} noValidate>
         <input type="text"
-               ref={this.textRef}
+               ref={textRef}
                className="input"
                placeholder="enter a player's name"
-               value={this.state.value}
-               onChange={this.handleValueChange}
+               value={value}
+               onChange={handleValueChange}
                required/>
         <input  type="submit" className="input" value="Add Player"/>
       </form>
     );
-  }
+
 }
 
 export default AddPlayerForm;
