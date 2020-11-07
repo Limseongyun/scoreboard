11417c7402588b5065989e23b33a68b104bcab42
SeongyunLim <lsy9699@naver.com>
2020-11-07 10:48:29 +0900

    useCustomhook 사용방법

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,39 +1,25 @@
 import React, {useEffect, useRef, useState} from "react";
+import {useInterval} from "../hooks/useInterval";
 
 function Stopwatch(props){
   let tickRef;
   const [isRunning,setIsRunning] = useState(false);
   const [timer,setTimer] = useState(0);
 
+  useInterval(()=>{
+   if(isRunning){
+     setTimer(timer+1);
+   }
+  },1000);
 
-  const refIsRunning = useRef(false);
 
-  
-  
-  const tick =() =>{
-    console.log(refIsRunning.current,timer);
-    if(refIsRunning.current){
-      setTimer(timer => timer+1)
-    }
-  }
-  
-  useEffect(()=>{
-    tickRef = setInterval(tick, 1000);
-    return()=>{
-      clearInterval(tickRef);
-    }
-    //리턴하면 사라지기전에 실행
-  },[]);//배열이 없으면 처음 한번만실행
-  //렌더링이 다시 일어난다 체크 해보자
 
   const getButton = () =>{
     if(isRunning){
-      refIsRunning.current = true;
       return (
         <button onClick={()=>{setIsRunning(!isRunning)}}>stop</button>
       )
     } else {
-      refIsRunning.current = false;
       return (
         <button onClick={()=>{setIsRunning(!isRunning)}}>start</button>
       )
